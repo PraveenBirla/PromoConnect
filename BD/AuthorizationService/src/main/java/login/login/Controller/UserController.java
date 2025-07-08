e3e7de6c07d1926175b7ca03e6894d25190cc2cd
pareveen-birla <birlap409@gmail.com>
2025-07-08 11:56:24 +0530

    update crossorigin

--- BD/AuthorizationService/src/main/java/login/login/Controller/UserController.java
+++ BD/AuthorizationService/src/main/java/login/login/Controller/UserController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-@CrossOrigin(origins=" http://localhost:5173") 
+@CrossOrigin(origins="https://promo-connect.vercel.app") 
 @RestController
 @RequestMapping("/user")
 public class UserController { 
