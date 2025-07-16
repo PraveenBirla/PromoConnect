1562295ae5d04de8d0cec49a93c425069312873a
pareveen-birla <birlap409@gmail.com>
2025-07-16 17:46:48 +0530

    update

--- BD/AuthorizationService/src/main/java/login/login/Model/User.java
+++ BD/AuthorizationService/src/main/java/login/login/Model/User.java
@@ -12,7 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity 
+@Entity  
 @Table(name="users")
 public class User {
     @Id
--- BD/AuthorizationService/src/main/java/login/login/configuration/CorsConfig.java
+++ BD/AuthorizationService/src/main/java/login/login/configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package login.login.configuration;
+ package login.login.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
      @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/user/**")  
-                .allowedOrigins("http://localhost:5173")  
+                .allowedOrigins("https://promo-connect.vercel.app")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowedHeaders("*")  
                 .allowCredentials(true);   
