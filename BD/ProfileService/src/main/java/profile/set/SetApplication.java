ce61e5dd69fab462eb9c89be9f729e455899c570
pareveen-birla <birlap409@gmail.com>
2025-07-25 23:28:40 +0530

    update the brandetail form profileservice test

--- BD/ProfileService/src/main/java/profile/set/SetApplication.java
+++ BD/ProfileService/src/main/java/profile/set/SetApplication.java
@@ -4,11 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication 
 public class SetApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {   
+
+		 Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()  
+                              .load();
+      String aivenPassword = dotenv.get("AIVEN_PASSWORD");
+
+			  if (aivenPassword != null) {
+            System.setProperty("AIVEN_PASSWORD", aivenPassword);
+        } else { 
+            System.err.println("⚠️ AIVEN_PASSWORD not found in .env file");
+        } 
+
+				
 		SpringApplication.run(SetApplication.class, args);
 	}
 
--- BD/ProfileService/src/test/java/profile/set/SetApplicationTests.java
+++ BD/ProfileService/src/test/java/profile/set/SetApplicationTests.java
@@ -1,13 +1,32 @@
 package profile.set;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest
+public class SetApplicationTests {
+   
+   @BeforeAll 
+    static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()
+                              .load();
+
+        String aivenPassword = dotenv.get("AIVEN_PASSWORD");
+        if (aivenPassword != null) {
+            System.setProperty("AIVEN_PASSWORD", aivenPassword);
+        } else {
+            System.err.println("❌ AIVEN_PASSWORD not found in .env file");
+        }
+    }
+
+    @Test
+    void contextLoads() {
+         
+    }
+
 
 }
